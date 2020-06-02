94c71b3d077777b7f0a1bcc7677f320db1d06333
aaryankotharii <aaryan.kothari@gmail.com>
2020-06-02 22:54:13 +0530

    SignUpView added

---
+++ ChatApp/SignUpView.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpView.swift
+//  ChatApp
+//
+//  Created by Aaryan Kothari on 02/06/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
