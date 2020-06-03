eab4d24fd5b0b99cf8188e9316f457ec655353a6
aaryankotharii <aaryan.kothari@gmail.com>
2020-06-03 09:24:01 +0530

    UserData model adde

---
+++ ChatApp/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  ChatApp
+//
+//  Created by Aaryan Kothari on 03/06/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+
+class UserData: NSObject {
+    var email : String?
+    var name : String?
+    var profileImageUrl : String?
+    var id : String?
+}
