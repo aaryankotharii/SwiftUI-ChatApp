5a360c10c69a7c1f5c74a48f71f68638e61cc7f7
aaryankotharii <aaryan.kothari@gmail.com>
2020-06-03 09:35:15 +0530

    ChatsView added

---
+++ ChatApp/View/ChatsView.swift
@@ -0,0 +1,21 @@
+//
+//  ChatsView.swift
+//  ChatApp
+//
+//  Created by Aaryan Kothari on 03/06/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ChatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatsView()
+    }
+}
