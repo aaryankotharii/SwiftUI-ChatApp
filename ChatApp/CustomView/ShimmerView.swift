4c8717f320531f3e554c0ecfe50f9b4bcaa45848
aaryankotharii <aaryan.kothari@gmail.com>
2020-06-06 21:58:50 +0530

    minor fix

---
+++ ChatApp/CustomView/ShimmerView.swift
@@ -0,0 +1,44 @@
+//
+//  ShimmerView.swift
+//  ChatApp
+//
+//  Created by Aaryan Kothari on 05/06/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShimmerView: View {
+    
+    private struct Constants {
+        static let duration: Double = 0.9
+        static let minOpacity: Double = 0.25
+        static let maxOpacity: Double = 1.0
+        static let cornerRadius: CGFloat = 2.0
+    }
+    
+    @State private var opacity: Double = Constants.minOpacity
+    
+    public init() {}
+    
+    public var body: some View {
+        RoundedRectangle(cornerRadius: Constants.cornerRadius)
+            .fill(Color.pink)
+            .opacity(opacity)
+            .transition(.opacity)
+            .onAppear {
+                let baseAnimation = Animation.easeInOut(duration: Constants.duration)
+                let repeated = baseAnimation.repeatForever(autoreverses: true)
+                withAnimation(repeated) {
+                    self.opacity = Constants.maxOpacity
+                }
+        }
+    }
+}
+
+
+struct ShimmerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShimmerView()
+    }
+}
